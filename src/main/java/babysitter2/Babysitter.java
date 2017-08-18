4090d65f410b4185b035a9060688f72ea6b52a4f
Cindy Colucy <ckcolucy@gmail.com>
2017-08-18 14:18:10 -0400

    "wrote first test for checking valid start time"

--- src/main/java/babysitter2/Babysitter.java
+++ src/main/java/babysitter2/Babysitter.java
@@ -4,14 +4,9 @@ import java.util.Scanner;
 
 public class Babysitter {
 
-	public static void main(String[] args) {
-
-		Scanner input = new Scanner(System.in);
-
-		System.out.println("Enter the start time in 24-hour convention, after 5pm.");
-
-		int startTime = input.nextInt();
-
+	public boolean isValidStartTime(int startTime) {
+		
+		return false;
 	}
 
 }
---
+++ src/main/java/babysitter2/BabysitterApp.java
@@ -0,0 +1,16 @@
+package babysitter2;
+
+import java.util.Scanner;
+
+public class BabysitterApp {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Enter the start time in 24-hour convention, after 5pm.");
+
+		int startTime = input.nextInt();
+
+	}
+
+}
